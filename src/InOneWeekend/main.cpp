acaf3acb73962626e0b5c429b68028683bc6399a
LylaT <lylatelkamp@gmail.com>
2025-11-04 21:05:29 -0800

    WIP commit to render ray
    Output image isn't a gradient and instead scan lines. Will investigate and drop this commit on fix

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -1,11 +1,38 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color rayColor(const ray& r) {
+    vec3 unitDirection = unitVector(r.direction());
+    auto a = 0.5*(unitDirection.y() + 1.0);
+    return (1.0-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main() {
+
+    auto aspectRatio = 16.0/9.0;
+
     int imageWidth = 256;
-    int imageHeight = 256;
+
+    int imageHeight = int(imageWidth / aspectRatio);
+    imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+
+    auto focalLength = 1.0;
+    auto viewportHeight = 2.0;
+    auto viewportWidth = viewportHeight * (double(imageWidth)/imageHeight);
+    auto cameraCenter = point3(0,0,0);
+
+    auto viewportU = vec3(viewportWidth, 0, 0);
+    auto viewportV = vec3(0, -viewportHeight, 0);
+
+    auto pixelDeltaU = viewportU / imageWidth;
+    auto pixelDeltaV = viewportV / imageHeight;
+
+    auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU/2 - viewportV/2;
+
+    auto pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
 
     std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
 
@@ -14,7 +41,10 @@ int main() {
         std::clog << "\rScanlines remaining:" << (imageHeight - j) << ' ' << std::flush;
         for (int i = 0; i < imageWidth; i++)
         {
-            auto pixelColor = color(double(i)/(imageWidth-1), double(j)/(imageHeight-1), 0);
+            auto pixelCenter = pixel00Loc + (i*pixelDeltaU) + (j*pixelDeltaV);
+            auto rayDirection = pixelCenter - cameraCenter;
+            ray r(cameraCenter, rayDirection);
+            color pixelColor = rayColor(r);
             writeColor(std::cout, pixelColor);
         }
     }
