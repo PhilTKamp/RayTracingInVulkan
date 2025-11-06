52f15dd0c46f401262103284b27c2c2c41853a25
LylaT <lylatelkamp@gmail.com>
2025-11-05 20:15:52 -0800

    Updating documentation in main.cpp

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -21,31 +21,49 @@ color rayColor(const ray& r, const hittable& world) {
 
 int main() {
 
-    auto aspectRatio = 16.0/9.0;
+    ////
+    // Image
+    ////
 
+    auto aspectRatio = 16.0/9.0;
     int imageWidth = 400;
 
+    // Calculate image height, min at 1
     int imageHeight = int(imageWidth / aspectRatio);
     imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+
+    ////
+    // World
+    ////
 
     hittableList world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    ////
+    // Camera
+    ////
 
     auto focalLength = 1.0;
     auto viewportHeight = 2.0;
     auto viewportWidth = viewportHeight * (double(imageWidth)/imageHeight);
     auto cameraCenter = point3(0,0,0);
 
+    // Calculate vectors along and down the viewport
     auto viewportU = vec3(viewportWidth, 0, 0);
     auto viewportV = vec3(0, -viewportHeight, 0);
 
+    // Determine delta for mapping pixels onto viewport
     auto pixelDeltaU = viewportU / imageWidth;
     auto pixelDeltaV = viewportV / imageHeight;
 
+    // Calculate upper left pixel location
     auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU/2 - viewportV/2;
-
     auto pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+
+    ////
+    // Render
+    ////
 
     std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
 
