3ab89ea15557f2df46a4429e33787e2da262318f
LylaT <lylatelkamp@gmail.com>
2025-11-05 19:39:34 -0800

    Adding hittable class

---
+++ src/InOneWeekend/hittable.h
@@ -0,0 +1,20 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class hitRecord {
+    public:
+        point3 p;
+        vec3 normal;
+        double t;
+};
+
+class hittable {
+    public:
+        virtual ~hittable() = default;
+
+        virtual bool hit(const ray& r, double tMin, double tMax, hitRecord& rec) const = 0;
+};
+
+#endif
