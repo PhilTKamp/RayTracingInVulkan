fce03f37065139826d02345f4894bc942ab5a9bb
LylaT <lylatelkamp@gmail.com>
2025-11-06 16:26:29 -0800

    Adding an upper bound to ray bounces

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -4,98 +4,110 @@
 #include "hittable.h"
 #include "color.h"
 
-class camera {
-    public:
-        double aspectRatio = 1.0;  // Ratio of image width over height
-        int    imageWidth  = 100;  // Rendered image width in pixel count
-        int samplesPerPixel = 10; // Count of random samples for each pixel
+class camera
+{
+public:
+    double aspectRatio = 1.0; // Ratio of image width over height
+    int imageWidth = 100;     // Rendered image width in pixel count
+    int samplesPerPixel = 10; // Count of random samples for each pixel
+    int maxDepth = 10;        // Maximum number of ray bounces in the scene
 
-        void render(const hittable& world) {
-            initialize();
+    void render(const hittable &world)
+    {
+        initialize();
 
-            std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+        std::cout << "P3\n"
+                  << imageWidth << " " << imageHeight << "\n255\n";
 
-            for (int j = 0; j < imageHeight; j++)
+        for (int j = 0; j < imageHeight; j++)
+        {
+            std::clog << "\rScanlines remaining:" << (imageHeight - j) << ' ' << std::flush;
+            for (int i = 0; i < imageWidth; i++)
             {
-                std::clog << "\rScanlines remaining:" << (imageHeight - j) << ' ' << std::flush;
-                for (int i = 0; i < imageWidth; i++)
+                color pixelColor(0, 0, 0);
+
+                for (int sample = 0; sample < samplesPerPixel; sample++)
                 {
-                    color pixelColor(0,0,0);
-
-                    for (int sample = 0; sample < samplesPerPixel; sample++)
-                    {
-                        ray r = getRay(i, j);
-                        pixelColor += rayColor(r, world);
-                    }
-                    
-                    writeColor(std::cout, pixelSamplesScale * pixelColor);
+                    ray r = getRay(i, j);
+                    pixelColor += rayColor(r, maxDepth, world);
                 }
+
+                writeColor(std::cout, pixelSamplesScale * pixelColor);
             }
-
-            std::clog << "\rDone.                   \n";        
         }
 
-    private:
-        int imageHeight;
-        double pixelSamplesScale; // Color scale factor for a sum of pixel samples
-        point3 center;
-        point3 pixel00Loc;
-        vec3 pixelDeltaU;
-        vec3 pixelDeltaV;
+        std::clog << "\rDone.                   \n";
+    }
 
-        void initialize() {
-            imageHeight = int(imageWidth / aspectRatio);
-            imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+private:
+    int imageHeight;
+    double pixelSamplesScale; // Color scale factor for a sum of pixel samples
+    point3 center;
+    point3 pixel00Loc;
+    vec3 pixelDeltaU;
+    vec3 pixelDeltaV;
 
-            pixelSamplesScale = 1.0 / samplesPerPixel;
+    void initialize()
+    {
+        imageHeight = int(imageWidth / aspectRatio);
+        imageHeight = (imageHeight < 1) ? 1 : imageHeight;
 
-            center = point3(0, 0, 0);
-            auto focalLength = 1.0;
-            auto viewportHeight = 2.0;
-            auto viewportWidth = viewportHeight * (double(imageWidth)/imageHeight);
-            auto cameraCenter = point3(0,0,0);
+        pixelSamplesScale = 1.0 / samplesPerPixel;
 
-            // Calculate vectors along and down the viewport
-            auto viewportU = vec3(viewportWidth, 0, 0);
-            auto viewportV = vec3(0, -viewportHeight, 0);
+        center = point3(0, 0, 0);
+        auto focalLength = 1.0;
+        auto viewportHeight = 2.0;
+        auto viewportWidth = viewportHeight * (double(imageWidth) / imageHeight);
+        auto cameraCenter = point3(0, 0, 0);
 
-            // Determine delta for mapping pixels onto viewport
-            pixelDeltaU = viewportU / imageWidth;
-            pixelDeltaV = viewportV / imageHeight;
+        // Calculate vectors along and down the viewport
+        auto viewportU = vec3(viewportWidth, 0, 0);
+        auto viewportV = vec3(0, -viewportHeight, 0);
 
-            // Calculate upper left pixel location
-            auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU/2 - viewportV/2;
-            pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+        // Determine delta for mapping pixels onto viewport
+        pixelDeltaU = viewportU / imageWidth;
+        pixelDeltaV = viewportV / imageHeight;
 
+        // Calculate upper left pixel location
+        auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
+        pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+    }
+
+    ray getRay(int i, int j) const
+    {
+
+        auto offset = sampleSquare();
+        auto pixelSample = pixel00Loc + ((i + offset.x()) * pixelDeltaU) + ((j + offset.y()) * pixelDeltaV);
+
+        auto rayOrigin = center;
+        auto rayDirection = pixelSample - rayOrigin;
+
+        return ray(rayOrigin, rayDirection);
+    }
+
+    vec3 sampleSquare() const
+    {
+        return vec3(randomDouble() - 0.5, randomDouble() - 0.5, 0);
+    }
+
+    color rayColor(const ray &r, int depth, const hittable &world) const
+    {
+        if (depth <= 0) {
+            return color(0, 0, 0);
         }
 
-        ray getRay(int i, int j) const {
+        hitRecord rec;
 
-            auto offset = sampleSquare();
-            auto pixelSample = pixel00Loc + ((i + offset.x()) * pixelDeltaU) + ((j + offset.y()) * pixelDeltaV);
-
-            auto rayOrigin = center;
-            auto rayDirection = pixelSample - rayOrigin;
-
-            return ray(rayOrigin, rayDirection);
-        }
-        
-        vec3 sampleSquare() const {
-            return vec3(randomDouble() - 0.5, randomDouble() - 0.5, 0);
+        if (world.hit(r, interval(0, infinity), rec))
+        {
+            vec3 direction = randomOnHemisphere(rec.normal);
+            return 0.5 * rayColor(ray(rec.p, direction), depth - 1, world);
         }
 
-        color rayColor(const ray& r, const hittable& world) const {
-            hitRecord rec;
-
-            if (world.hit(r, interval(0, infinity), rec)){
-                vec3 direction = randomOnHemisphere(rec.normal);
-                return 0.5 * rayColor(ray(rec.p, direction), world);
-            }
-            
-            vec3 unitDirection = unitVector(r.direction());
-            auto a = 0.5*(unitDirection.y() + 1.0);
-            return (1.0-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-        }
+        vec3 unitDirection = unitVector(r.direction());
+        auto a = 0.5 * (unitDirection.y() + 1.0);
+        return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+    }
 };
 
 #endif
