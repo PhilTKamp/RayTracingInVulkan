117a99ae8177cf4f2b57e324eebc8b892a69579d
LylaT <lylatelkamp@gmail.com>
2025-11-06 19:08:38 -0800

    Incorporating dielectric into scene

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -13,7 +13,7 @@ int main() {
 
     auto materialGround = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto materialCenter = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto materialLeft = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto materialLeft = make_shared<dielectric>(1.5);
     auto materialRight = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, materialGround));
