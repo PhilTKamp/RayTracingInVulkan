e5bde2deb1a94c0061f2db9d978ffd5b21f0abb5
LylaT <lylatelkamp@gmail.com>
2025-11-06 16:41:26 -0800

    Adding an abstract material base class

---
+++ src/InOneWeekend/material.h
@@ -0,0 +1,17 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "hittable.h"
+#include "color.h"
+#include "ray.h"
+
+class material {
+    public:
+        virtual ~material() = default;
+
+        virtual bool scatter(const ray& rIn, const hitRecord& rec, color& attenuation, ray& scattered) const {
+            return false;
+        }
+};
+
+#endif
