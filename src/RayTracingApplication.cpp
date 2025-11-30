a66fc1e463a8e5d4a28b1d0b580cdb5274d641de
LylaT <lylatelkamp@gmail.com>
2025-11-29 19:41:52 -0800

    Variety of changes to fix clangd and vulkan

--- src/RayTracingApplication.cpp
+++ src/RayTracingApplication.cpp
@@ -1,5 +1,28 @@
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #include "RayTracingApplication.hpp"
+#include "vulkan/vulkan.hpp"
 #include <iostream>
 #include <ostream>
 
 void RayTracingApplication::run() { std::cout << "Running!" << std::endl; }
+
+void RayTracingApplication::initVulkan() {
+  std::cout << "Intializing Vulkan..." << std::endl;
+}
+
+void RayTracingApplication::createInstance() {
+  constexpr vk::ApplicationInfo appInfo{
+      .pApplicationName = "Ray Tracer",
+      .pEngineName = "No Engine",
+      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+      .apiVersion = vk::ApiVersion14,
+  };
+}
+
+void RayTracingApplication::mainLoop() {
+  std::cout << "Running the main loop..." << std::endl;
+}
+
+void RayTracingApplication::cleanup() {
+  std::cout << "Cleaning Up..." << std::endl;
+}
--- src/RayTracingApplication.hpp
+++ src/RayTracingApplication.hpp
@@ -1,9 +1,20 @@
 #ifndef RAY_TRACING_APPLICATION_H
 #define RAY_TRACING_APPLICATION_H
 
+#include <vulkan/vulkan_raii.hpp>
+
 class RayTracingApplication {
 public:
   void run();
+
+private:
+  void initVulkan();
+  void mainLoop();
+  void cleanup();
+
+  vk::raii::Instance instance;
+
+  void createInstance();
 };
 
 #endif
